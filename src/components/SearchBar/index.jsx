810841c72db76eb0cfde6ce6860ecf14e6d49116
Nimrod Eshel <nimrod.eshel@outlook.com>
2022-11-20 19:28:30 -0500

    initial commit

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,25 @@
+import './SearchBar.scss'
+import { useState } from "react";
+import { useAppContext } from "../../context/appContext";
+import { useAsync } from "../../hooks/useAsync";
+export const SearchBar = () => {
+	const [searchValue, setSearchValue] = useState()
+	const {searchImageList, loadImageList} = useAppContext()
+	const handleSetSearchValue = (event) => {
+		setSearchValue(event.target.value)
+	}
+	const handleSearchGiphy = async () => {
+		if(searchValue.length > 0) {
+			await searchImageList({q: searchValue})
+		} else {
+			await loadImageList()
+		}
+	}
+	const {execute: refreshList} = useAsync({asyncFunction: handleSearchGiphy, immediate:false})
+
+ return <div className='search-bar'>
+	 <label for='search'>Search Gifs!</label>
+	 <input onChange={handleSetSearchValue} id='search' type={'search'} />
+	 <button onClick={refreshList}>Search</button>
+ </div>
+}
